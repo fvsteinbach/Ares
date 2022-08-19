00c3346aaeed2cf6b5042f0e3116ce0cd7b17743
Fernando Vinicius <99344303+fvsteinbach@users.noreply.github.com>
2022-08-19 00:08:18 -0300

    Created page to register

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
 if __name__==("__main__"):
     app.run(debug=True)
