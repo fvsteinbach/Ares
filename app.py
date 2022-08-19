3ba2b3f40d4f878be14e796887f8e432f5139237
Fernando Vinicius <99344303+fvsteinbach@users.noreply.github.com>
2022-08-19 00:21:26 -0300

    Updated

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
+from crypt import methods
 from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
     return render_template("register.html")
 
