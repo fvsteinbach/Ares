d9044b96ec8829fc21b5f285c09cf2f9c2805fd5
Fernando Vinicius <99344303+fvsteinbach@users.noreply.github.com>
2022-08-19 14:34:46 -0300

    Implemented data validation

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
+fighters = {}
+
+BELTS = ["No belt", "White", "Blue", "Purple", "Brown", "Black"]
+DEGREES = ["No degree","I", "II", "III", "IV"]
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
@@ -24,6 +29,24 @@ def registrants():
     birthdate = request.form.get("birthdate")
     if not birthdate:
         return render_template("error.html", message="Missing birthdate")
+    
+    #Validate fighter belt
+    belt = request.form.get("belt")
+    if belt not in BELTS:
+        return render_template("error.html", message="Missing belt")
+    
+    #Validate fighter belt degree
+    degree = request.form.get("degree")
+    if degree not in DEGREES:
+        return render_template("error.html", message="Missing belt degree")
+
+    #Validate email
+    email = request.form.get("email")
+    if not email:
+        return render_template("error.html", message="Missing email")
+        
+    fighters[name] = belt
+
     return render_template("registrants.html", name=name)
 
 if __name__==("__main__"):
