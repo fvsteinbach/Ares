b195b873ba0894a3293cc34fbc62fd504769877e
Fernando Vinicius <99344303+fvsteinbach@users.noreply.github.com>
2022-08-20 04:06:36 -0300

    Installed new libraries and implemented sessions

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, request, render_template, redirect, session
 from cs50 import SQL
 from datetime import date
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, EmailField, TelField
+from wtforms.validators import data_required,input_required
+from flask_session import Session
 
+#Configure app
 app = Flask(__name__)
 
+#Configure database
 db = SQL("sqlite:///students.db")
 
+#Configure session
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 BELTS = ["No belt", "White", "Blue", "Purple", "Brown", "Black"]
 DEGREES = ["No degree","I", "II", "III", "IV"]
@@ -14,15 +24,23 @@ DEGREES = ["No degree","I", "II", "III", "IV"]
 def index():
     return render_template("index.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     return render_template("register.html", belts=BELTS, degrees=DEGREES)
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
 
 @app.route("/error")
 def error():
     return render_template("error.html")
 
-@app.route("/registrants", methods=["POST"])
+@app.route("/registrants", methods=["POST","GET"])
 def registrants():
     #Validate first name
     fname = request.form.get("fname")
@@ -59,6 +77,7 @@ def registrants():
     if not phone:
         return render_template("error.html", message="Missing phone")
 
+
     #Get student age
     birthyear = int(birthdate[:4])
     thisyear = date.today().year
@@ -67,16 +86,22 @@ def registrants():
     #Remember student
     db.execute("INSERT INTO students (fname, lname, belt, degree, age, phone, email) VALUES (?, ?, ?, ?, ?, ?, ?)", fname, lname, belt, degree, age, phone, email)
 
+    #Saves the data on a session
+    if request.method == "POST":
+        session["fname"] = request.form.get("fname")
+        return redirect("/dashboard")
+
     #Confirm registration
-    return redirect("/dashboard")
+    return render_template("user.html", fname=fname)
 
 @app.route("/user", methods=["POST", "GET"])
 def user():
-    fname = request.form.get("fname")
-    return render_template("/user.html", fname=fname)
+    return render_template("/user.html")
 
 @app.route("/dashboard")
 def dashboard():
+    if not session.get("fname"):   
+        return redirect("/register")
     students = db.execute("SELECT * FROM students")
     return render_template("dashboard.html", students=students)
 
